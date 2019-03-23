8159157ad23eabefe12fe1a0f914e9ba120981d7
Andrei <andreiburko99@gmail.com>
2019-03-24 01:07:27 +0300

    feat: - complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let array = str.split('');
+  let bracket = bracketsConfig;
+  for(let i=0;i<arr.length;i++){
+    
+    for(let j=0; j<bracket.length; j++){
+      if(array[i] === bracket[j][0] && array[i+1] === bracket[j][1]){
+        array.splice(i,2);
+        if (array.length==0){ 
+            return true;
+        }
+        i=-1;
+      }
+    }
+  }
+  return false;
 }
